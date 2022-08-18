5e14662961ec7aa950d6ce876e48d600f40cc5ed
tvondollen <Thomas.VonDollen@its.ny.gov>
2022-08-17 20:48:15 -0400

    Finished "Style the App" module

--- Bullseye/app/src/main/java/com/example/bullseye/MainActivity.kt
+++ Bullseye/app/src/main/java/com/example/bullseye/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        supportActionBar?.hide() //hides top bar
+
 
         //inflate() converts XML into View object
         binding = ActivityMainBinding.inflate(layoutInflater)
